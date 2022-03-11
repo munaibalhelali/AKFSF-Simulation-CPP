49d0a3739923dbfb38d1aa2b2d66b978d47be9f9
munaibalhelali <moneebalhelaly@yahoo.com>
2022-03-11 16:33:53 +0100

    solved lkf tasks

--- src/kalmanfilter_lkf_student.cpp
+++ src/kalmanfilter_lkf_student.cpp
@@ -11,11 +11,12 @@
 
 // -------------------------------------------------- //
 // YOU CAN USE AND MODIFY THESE CONSTANTS HERE
-constexpr bool INIT_ON_FIRST_PREDICTION = true;
+constexpr bool INIT_ON_FIRST_PREDICTION = false;
 constexpr double INIT_POS_STD = 0;
-constexpr double INIT_VEL_STD = 0;
+constexpr double INIT_VEL_STD = 15;
 constexpr double ACCEL_STD = 0;
 constexpr double GPS_POS_STD = 3.0;
+constexpr double PI = 3.14159;
 // -------------------------------------------------- //
 
 void KalmanFilter::predictionStep(double dt)
@@ -35,7 +36,15 @@ void KalmanFilter::predictionStep(double dt)
 
             // Assume the initial position is (X,Y) = (0,0) m
             // Assume the initial velocity is 5 m/s at 45 degrees (VX,VY) = (5*cos(45deg),5*sin(45deg)) m/s
+            state[0] = 0;
+            state[1] = 0;
+            state[2] = 5*cos(PI/4);
+            state[3] = 5*sin(PI/4);
 
+            cov(0,0) = INIT_POS_STD*INIT_POS_STD;
+            cov(1,1) = INIT_POS_STD*INIT_POS_STD;
+            cov(2,2) = INIT_VEL_STD*INIT_VEL_STD;
+            cov(3,3) = INIT_VEL_STD*INIT_VEL_STD;
             setState(state);
             setCovariance(cov);
         // ----------------------------------------------------------------------- //
@@ -51,8 +60,31 @@ void KalmanFilter::predictionStep(double dt)
         // Hint: You can use the constants: ACCEL_STD
         // ----------------------------------------------------------------------- //
         // ENTER YOUR CODE HERE
+        MatrixXd F = Matrix4d::Zero();
+        for(int i =0; i<4;i++){
+            F(i,i) = 1;
+        }
+        F(0,2) = dt;
+        F(1,3) = dt;
+        state = F*state;
 
+        MatrixXd Q = Matrix2d::Zero();
+        Q(0,0) = (ACCEL_STD*ACCEL_STD);
+        Q(0,1) = 0; 
+        Q(1,0) = 0;
+        Q(1,1) = (ACCEL_STD*ACCEL_STD);
 
+        MatrixXd L = MatrixXd(4,2);
+        L(0,0) = (0.5*dt*dt);
+        L(0,1) = 0; 
+        L(1,0) = 0;
+        L(1,1) = (0.5*dt*dt);
+        L(2,0) = dt;
+        L(2,1) = 0; 
+        L(3,0) = 0;
+        L(3,1) = dt;
+
+        cov = F*cov*F.transpose()+L*Q*L.transpose();
         // ----------------------------------------------------------------------- //
 
         setState(state);
@@ -73,7 +105,23 @@ void KalmanFilter::handleGPSMeasurement(GPSMeasurement meas)
         // Hint: You can use the constants: GPS_POS_STD
         // ----------------------------------------------------------------------- //
         // ENTER YOUR CODE HERE 
+        MatrixXd H = MatrixXd(2, 4);
+        H << 1,0,0,0,0,1,0,0;
 
+        MatrixXd R = MatrixXd(2,2);
+        R << (GPS_POS_STD*GPS_POS_STD),0,0,(GPS_POS_STD*GPS_POS_STD);
+
+        MatrixXd z = MatrixXd(2,1);
+        z<<meas.x, meas.y;
+        
+        MatrixXd y = z - H * state;
+        MatrixXd S = H * cov * H.transpose() + R;
+        MatrixXd K = cov * H.transpose() * S.inverse();
+
+        state = state + K*y;
+        MatrixXd I = MatrixXd(4,4);
+        I <<1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1;
+        cov = (I-K*H)*cov;
 
         // ----------------------------------------------------------------------- //
 
@@ -92,7 +140,14 @@ void KalmanFilter::handleGPSMeasurement(GPSMeasurement meas)
             VectorXd state = Vector4d::Zero();
             MatrixXd cov = Matrix4d::Zero();
 
+            // Assume the initial position is (X,Y) = (0,0) m
+            // Assume the initial velocity is 5 m/s at 45 degrees (VX,VY) = (5*cos(45deg),5*sin(45deg)) m/s
+            state <<meas.x, meas.y, 0, 0;
 
+            cov(0,0) = (GPS_POS_STD*GPS_POS_STD);
+            cov(1,1) = (GPS_POS_STD*GPS_POS_STD);
+            cov(2,2) = INIT_VEL_STD*INIT_VEL_STD;
+            cov(3,3) = INIT_VEL_STD*INIT_VEL_STD;
             setState(state);
             setCovariance(cov);
         // ----------------------------------------------------------------------- //
